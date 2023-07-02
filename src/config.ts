3b37bede2242361652885ba9ea10aca47c604d1d
Patricia Navarro <navarropatricia15@yahoo.com.ph>
2023-07-02 18:38:29 +0800

    Github action workflow init

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://patmln.github.io/",
+  author: "Pat Navarro",
+  desc: "Pat's blog",
+  title: "Pat's blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
